e1fd82d0f78d9b7e1039af6bb0bf7749968b6a2b
Cat Hallam <catherinehallam@mutumojaa.com>
2018-07-01 16:23:24 +0100

    Create Ship Set Sail Method

--- __test__/Ship.test.js
+++ __test__/Ship.test.js
@@ -2,14 +2,14 @@
 //import constructor file
 const Ship = require('../src/Ship.js');
 
-//describe the Ship constructor:
+//describing the Ship constructor:
 describe('Ship', () => {
     it('creates an object instance', () => {
      expect(new Ship()).toBeInstanceOf(Object);
  });
 });
 
-//Starting Port
+//Starting Port constructor
 describe('Port', () =>{
     it('has a starting port', () => {
         const ship = new Ship('Southampton');
@@ -17,4 +17,17 @@ describe('Port', () =>{
     });
 });
 
+//Ship is set to sail from port
+describe('Sail', () => {
+    it('setting sail', () => {
+        const ship = new Ship('Southampton');
+        //.sailMethod is created
+        ship.setSail();
+        expect(ship.startingPort).toBeFalsy();
+    });
+}
+)
+Ship.prototype.setSail = function() {
+   
+  };
 
