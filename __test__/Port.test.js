8455c98c190f05a7d383e91baf5de31ee61b07c7
Cat Hallam <catherinehallam@mutumojaa.com>
2018-07-15 23:01:58 +0100

    Step 6 - Ports to keep track of ships currently docked

--- __test__/Port.test.js
+++ __test__/Port.test.js
@@ -5,12 +5,28 @@ describe('Port', () => {
   it('creates an object instance', () => {
     expect(new Port()).toBeInstanceOf(Object);
   });
-});
-
-// A   new test spec to check the new Port object has a name property.
-describe('Port', () =>{
+  // A  new test spec to check the new Port object has a name property.
   it('name', () => {
     const port = new Port('Rome');
     expect(port.name).toBe('Rome');
   });
+});
+
+// Port addShip Method to keep track of ships so that we can add more ships using an array
+it('add new ship', () => {
+  const port = new Port('Southampton');
+  const ship = {};
+  port.addShip(ship);
+  expect(port.ships).toContain(ship);
+});
+
+// Port removeShip method to keep track of ships not sailing
+it('remove ship', () => {
+  const port = new Port('Southampton');
+  const cunard = {};
+  const seabourn = {};
+  port.addShip(cunard);
+  port.addShip(seabourn);
+  port.removeShip(seabourn);
+  expect(port.ships).toEqual([cunard]);
 });
