90ddfb4fe1fee94c3cdeec104208fd12b61e6927
Cat Hallam <catherinehallam@mutumojaa.com>
2018-07-15 15:04:40 +0100

    Step 5 - Ship's itinerary determines at which port it next docks

--- __test__/Ship.test.js
+++ __test__/Ship.test.js
@@ -1,40 +1,60 @@
 // import constructor file
 const Ship = require('../src/Ship.js');
 const Port = require('../src/Port.js');
+const Itinerary = require('../src/Itinerary.js');
 
-// describing the Ship constructor:
+// describing the Ship constructor and check the new Ship object can be instantiated
 describe('Ship', () => {
-  it('creates an object instance', () => {
-    expect(new Ship()).toBeInstanceOf(Object);
+  it('creates an new object instance', () => {
+    const port = new Port('Southampton');
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
+    expect(ship).toBeInstanceOf(Object);
   });
-});
-
-// Starting Port constructor
-describe('Port', () =>{
+  /* Describe a new port object and instantiate the new Port passing in 'Southampton'
+  as its name then pass the new instance into Ship ( the word port is a dependency inversion */
   it('has a starting port', () => {
     const port = new Port('Southampton');
-    const ship = new Ship(port);
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
     expect(ship.currentPort).toBe(port);
   });
 });
 
-// Ship is set to sail from port
+// Ship is set to sail from port and then to multiple ports in the itinerary
 describe('Sail', () => {
   it('setting sail', () => {
-    const port = new Port('Southampton');
-    const ship = new Ship(port);
+    const southampton = new Port('Southampton');
+    const rome = new Port('Rome');
+    const itinerary = new Itinerary([southampton, rome]);
+    const ship = new Ship(itinerary);
     ship.setSail();
     expect(ship.currentPort).toBeFalsy();
   });
 });
 
-// Ship new dock
+/* Refactor the Ship test suite so a Ship takes a Port object instead of a string then
+ create a new test spec inside Ship.test.js to test a dock method */
 describe('Dock', () => {
   it('can dock at a different port', () => {
     const southampton = new Port('Southampton');
-    const ship = new Ship(southampton);
     const rome = new Port('Rome');
-    ship.dock(rome);
+    const itinerary = new Itinerary([southampton, rome]);
+    const ship = new Ship(itinerary);
+    ship.setSail();
+    ship.dock();
     expect(ship.currentPort).toEqual(rome);
+  });
+});
+
+describe('Sail', () => {
+  it('it cannot sail further than its itinerary', () => {
+    const southampton = new Port('Southampton');
+    const rome = new Port('Rome');
+    const itinerary = new Itinerary([southampton, rome]);
+    const ship = new Ship(itinerary);
+    ship.setSail();
+    ship.dock();
+    expect(() => ship.setSail()).toThrowError('End of itinerary reached');
   });
 });
--- src/Ship.js
+++ src/Ship.js
@@ -1,16 +1,37 @@
-function Ship(currentPort, passengers) {
-  this.currentPort = currentPort;
-  this.passengers = passengers;
-};
+// created constructors to ensure  the tests to pass
+function Ship(itinerary) {
+  this.itinerary = itinerary;
+  this.currentPort = itinerary.ports[0];
+  this.previousPort = null;
+}
 
-Ship.prototype.setSail = function () {
+// Create a .setsail method
+Ship.prototype.setSail = function setSail() {
+  const itinerary = this.itinerary;
+  const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+
+  // Check if we are at the end of the itinerary.
+  // If we are we don't want to set stail, so raise an error
+  if ((currentPortIndex) === (itinerary.ports.length - 1)) {
+    throw new Error('End of itinerary reached');
+  }
+
+  // We have set sail, so are no longer at a port. Set the previous port
+  // to the one we just left and the current port to nothing, as we
+  // are at sea
+  this.previousPort = this.currentPort;
   this.currentPort = null;
 };
 
-// Dock at a different port.
-// Ship.prototype.dock = function () {
-Ship.prototype.dock = function dock(port) {
-  this.currentPort = port;
+// Create a dock method to ensure the ship docks at different ports
+Ship.prototype.dock = function dock() {
+  const itinerary = this.itinerary;
+  // identify the position in the itinerary of the previous port
+  const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+  // get the port the ship is docking at from the itinerary
+  // Increment the previous port index to identify the new port in the itinerary
+  this.currentPort = itinerary.ports[previousPortIndex + 1];
 };
+
 
 module.exports = Ship;
